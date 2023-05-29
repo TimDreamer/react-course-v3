da107eff8026a450b0f15ffed237732cc45d4fe1
Tim Lu <tim.lu@linecrop.com>
2023-05-29 17:10:40 +0800

    3rd basic exercise

--- 03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,32 @@
-const UseStateArray = () => {
-  return <h2>useState array example</h2>;
-};
+import React, { useState } from 'react'
+import { data } from '../../../data'
 
-export default UseStateArray;
+const UseStateArray = () => {
+  const [people, setPeople] = useState(data)
+  const removePerson = (id) => {
+    setPeople(people.filter((person) => person.id !== id))
+  }
+
+  return (
+    <div>
+      {people.map((person) => {
+        const { id, name } = person
+        return (
+          <React.Fragment key={id}>
+            <h4>{name}</h4>
+            <button onClick={() => removePerson(id)}>remove</button>
+          </React.Fragment>
+        )
+      })}
+      <button
+        style={{ display: 'block', margin: 'auto' }}
+        className="btn"
+        onClick={() => setPeople([])}
+      >
+        clear all
+      </button>
+    </div>
+  )
+}
+
+export default UseStateArray
