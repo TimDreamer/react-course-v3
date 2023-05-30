94fbfef330aa1e0859da76c103e10bf4d7672530
Tim Lu <tim.lu@linecrop.com>
2023-05-30 17:43:29 +0800

    4th basic exercise

--- 03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,32 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
-};
+  const [people, setPeople] = useState({
+    name: 'Anna',
+    age: 24,
+    hobby: 'camping',
+  })
 
-export default UseStateObject;
+  const setJohn = () => {
+    setPeople({
+      name: 'John',
+      age: 30,
+      hobby: 'coding',
+    })
+  }
+  return (
+    <div>
+      <h1>{people.name}</h1>
+      <h2>{people.age}</h2>
+      <h3>Enjoys To: {people.hobby}</h3>
+      <button
+        onClick={setJohn}
+        className="btn"
+      >
+        show John
+      </button>
+    </div>
+  )
+}
+
+export default UseStateObject
